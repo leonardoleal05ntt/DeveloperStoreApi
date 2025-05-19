520e337ade466d05f12ac104101681672700762e
leonardoleal05ntt <llealmel@emeal.nttdata.com>
2025-05-19 09:35:10 -0300

    implementando controller de vendas

---
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/CreateSale/CreateSaleRequest.cs
@@ -0,0 +1,12 @@
+﻿using Ambev.DeveloperEvaluation.Application.Sales.CreateSale;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Sales.CreateSale
+{
+    public class CreateSaleRequest
+    {
+        public string SaleNumber { get; set; }
+        public Guid CustomerId { get; set; }
+        public Guid BranchId { get; set; }
+        public List<CreateSaleItemRequest> Items { get; set; }
+    }
+}
---
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/SalesController.cs
@@ -0,0 +1,54 @@
+﻿using Ambev.DeveloperEvaluation.Application.Sales.CreateSale;
+using Ambev.DeveloperEvaluation.WebApi.Common;
+using Ambev.DeveloperEvaluation.WebApi.Features.Sales.CreateSale;
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Sales
+{
+    /// <summary>
+    /// Controller for managing user operations
+    /// </summary>
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SalesController : BaseController
+    {
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+        public SalesController(IMediator mediator, IMapper mapper)
+        {
+            _mediator = mediator;
+            _mapper = mapper;
+        }
+
+        /// <summary>
+        /// Creates a new sale
+        /// </summary>
+        /// <param name="request">The user creation request</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>The created user details</returns>
+        [HttpPost]
+        [ProducesResponseType(typeof(ApiResponseWithData<Guid>), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> CreateUser([FromBody] CreateSaleRequest request, CancellationToken cancellationToken)
+        {
+            var validator = new CreateSaleRequestValidator();
+            var validationResult = await validator.ValidateAsync(request, cancellationToken);
+
+            if (!validationResult.IsValid)
+                return BadRequest(validationResult.Errors);
+
+            var command = _mapper.Map<CreateSaleCommand>(request);
+            var response = await _mediator.Send(command, cancellationToken);
+
+            return Created(string.Empty, new ApiResponseWithData<Guid>
+            {
+                Success = true,
+                Message = "Sale created successfully",
+                Data = response
+            });
+        }
+
+    }
+}

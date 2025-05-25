fee0d79f657bef358c8538173090e73fbdd6ab28
Leonardo Leal <leonardo.leal@fieldcorp.com.br>
2025-05-25 17:49:19 -0300

    implementando método para edição e listagem de unidadee no repository

--- src/Ambev.DeveloperEvaluation.ORM/Repositories/BranchRepository.cs
+++ src/Ambev.DeveloperEvaluation.ORM/Repositories/BranchRepository.cs
@@ -1,4 +1,5 @@
-﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+﻿using Ambev.DeveloperEvaluation.Domain.Common;
+using Ambev.DeveloperEvaluation.Domain.Entities;
 using Ambev.DeveloperEvaluation.Domain.Repositories;
 using Microsoft.EntityFrameworkCore;
 
@@ -23,6 +24,38 @@ namespace Ambev.DeveloperEvaluation.ORM.Repositories
         public async Task<Branch?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
         {
             return await _context.Branches.FirstOrDefaultAsync(o => o.Id == id, cancellationToken);
+        }
+
+        public async Task UpdateAsync(Branch branch, CancellationToken cancellationToken = default)
+        {
+            _context.Branches.Update(branch);
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task<PagedResult<Branch>> GetPagedAsync(int pageNumber, int pageSize, string? search = null, bool? active = null, CancellationToken cancellationToken = default)
+        {
+            IQueryable<Branch> query = _context.Branches;
+
+            if (!string.IsNullOrWhiteSpace(search))
+            {
+                query = query.Where(c =>
+                    c.Name.Contains(search));
+            }
+
+            if (active.HasValue)
+                query = query.Where(c => c.Active == active.Value);
+
+            int totalCount = await query.CountAsync(cancellationToken);
+            var items = await query
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync(cancellationToken);
+
+            return new PagedResult<Branch>
+            {
+                Items = items,
+                TotalCount = totalCount
+            };
         }
     }
 }
